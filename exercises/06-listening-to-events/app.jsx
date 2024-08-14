6409726324333862846ef907cacfccdc5b6b076e
Melissa Ortiz <melissa.ortiz.m88@gmail.com>
2024-08-13 20:07:35 -0400

    practice terminado

--- exercises/06-listening-to-events/app.jsx
+++ exercises/06-listening-to-events/app.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const clickHandler = e => {
+const clickHandler = (e) => {
 	console.log("I was clicked!", e);
 };
 
 const Alert = () => {
-	return <button className="btn btn-success btn-lg">Click Me</button>;
+	return (
+		<button className="btn btn-success btn-lg" onClick={clickHandler}>
+			Click Me
+		</button>
+	);
 };
 
 // You don't have to edit anything below
